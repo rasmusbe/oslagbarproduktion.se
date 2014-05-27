6822d7d02c63c5b7efddb973de2aa302e7ea3a31
Rasmus Bengtsson <raaz@Rasmus-MacBook-Pro.local>
2014-05-27 14:53:08 +0200

    Optimering

--- js/site.js
+++ js/site.js
@@ -1,12 +1,65 @@
-$(function() {
-	$('.obfuscated-email').each(function() {
-		var $this = $(this);
-		var em = $this.data('id') + '@oslagbar' + 'produktion.se';
+// matches(el, '.my-class');
+var matches = function(el, selector) {
+	var _matches = (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector);
 
-		$this.text(em);
+	if (_matches) {
+		return _matches.call(el, selector);
+	}
+	else {
+		var nodes = el.parentNode.querySelectorAll(selector);
 
-		if ($this.is('a')) {
-			$this.attr('href', 'mailto:' + em);
+		for (var i = nodes.length; i--;) {
+			if (nodes[i] === el) {
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+// ready( function () {} );
+var ready = function (fn) {
+	if (document.readyState === "complete" || document.readyState === "interactive") {
+		fn();
+	}
+	else if (document.addEventListener) {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+	else {
+		document.attachEvent('onreadystatechange', function() {
+			if (document.readyState === "complete" || document.readyState === 'interactive') {
+				fn();
+			}
+		});
+	}
+};
+
+// setText(element, string);
+var setText = function (el, string) {
+	if (el.textContent !== undefined) {
+		el.textContent = string;
+	}
+	else {
+		el.innerText = string;
+	}
+};
+
+// forEachElement(selector, function(el, i) { });
+var forEachElement = function (selector, fn) {
+	var elements = document.querySelectorAll(selector);
+	for (var i = 0; i < elements.length; i++) {
+		fn(elements[i], i);
+	}
+};
+
+ready(function(){
+	forEachElement('.obfuscated-email', function(el) {
+		var em = el.getAttribute('data-id')  + '@oslagbar' + 'produktion.se';
+
+		setText(el, em);
+
+		if (matches(el, 'a')) {
+			el.setAttribute('href', 'mailto:' + em);
 		}
 	});
 });
